77a9e832595ba635f44be146770418accd292577
GKMODSVIPER <gkmodsviper@gmail.com>
2024-09-04 18:02:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7293932407:AAFEZpsLMgxk8AXT_XpTJGdpRYp2ZAPBJ20")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("vipergkmods")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6496055615")
 
 
 
